e3603101984a65fb7b1f5861d6bbb4aa383fca39
Juan Delgado <soy.juan.delgado@gmail.com>
2025-06-30 13:00:29 +0200

    change files estructure

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Scene } from './scene/scene';
+
+@Component({
+  selector: 'app-home',
+  imports: [Scene],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
