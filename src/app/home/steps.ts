5ac781db30e1784c540011d935578244633dd89f
Juan Delgado <soy.juan.delgado@gmail.com>
2025-07-01 13:18:33 +0200

    show all scenes from steps

--- src/app/home/steps.ts
+++ src/app/home/steps.ts
@@ -5,11 +5,26 @@ import { IStep } from './istep';
   providedIn: 'root',
 })
 export class Steps {
-  steps: IStep[] = [];
-  current = 0;
-  constructor() {}
+  steps: IStep[] = [
+    {
+      title: 'Dedica moltes hores',
+      description: `Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.`,
+      img: '',
+      bgcolor: '',
+    },
+    {
+      title: 'Programa projectes propis:',
+      description: `Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.`,
+      img: '',
+      bgcolor: '',
+    },
+    {
+      title: 'Procura descansar:',
+      description: `Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.`,
+      img: '',
+      bgcolor: '',
+    },
+  ];
 
-  getCurrent() {
-    return this.current;
-  }
+  constructor() {}
 }
